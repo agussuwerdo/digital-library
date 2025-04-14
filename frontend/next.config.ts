c40c36e3e35761ad24214c8af2cc94dcf75af84a
Agus Suwerdo <agussuwerdo@gmail.com>
2025-04-14 20:26:33 +0700

    update next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
   /* config options here */
 };
 
